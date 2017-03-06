557ea700539569d4172ff7ba6bacbfd178d1b17b
edward <edward@cs.uoregon.edu>
2017-03-05 17:08:18 -0800

    Modifying former images

--- login.php
+++ login.php
@@ -113,38 +113,84 @@
 			</div>
 		</div>
 		<hr style="border-top: 2px solid #fff;">        
-        <div class="container" style=" ">
-		<button id="createGoalBtn" class="btn btn-primary portfolio-link" >New Goal</button> 
-	<a href="achievements.php" class="btn btn-primary">Achievements</a>
         <P></p>
+
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<div class="" id="loginModal">
+					  <div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<h3>Have an Account?</h3>
+					  </div>
+					  <div class="modal-body">
+						<div class="well">
+						  <ul class="nav nav-tabs">
+							<li class="active"><a href="#login" data-toggle="tab">Login</a></li>
+							<li><a href="#create" data-toggle="tab">Create Account</a></li>
+						  </ul>
+						  <div id="myTabContent" class="tab-content">
+							<div class="tab-pane active in" id="login">
+							  <form class="form-horizontal" action='' method="POST">
+								<fieldset>
+								  <div id="legend">
+									<legend class="">Login</legend>
+								  </div>    
+								  <div class="control-group">
+									<!-- Username -->
+									<label class="control-label"  for="username">Username</label>
+									<div class="controls">
+									  <input type="text" id="username" name="username" placeholder="" class="input-xlarge">
+									</div>
+								  </div>
+			 
+								  <div class="control-group">
+									<!-- Password-->
+									<label class="control-label" for="password">Password</label>
+									<div class="controls">
+									  <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+									</div>
+								  </div>
+			 
+			 
+								  <div class="control-group">
+									<!-- Button -->
+									<div class="controls">
+									  <button class="btn btn-success">Login</button>
+									</div>
+								  </div>
+								</fieldset>
+							  </form>                
+							</div>
+							<div class="tab-pane fade" id="create">
+							  <form id="tab">
+								<label>Username</label>
+								<input type="text" value="" class="input-xlarge">
+								<label>First Name</label>
+								<input type="text" value="" class="input-xlarge">
+								<label>Last Name</label>
+								<input type="text" value="" class="input-xlarge">
+								<label>Email</label>
+								<input type="text" value="" class="input-xlarge">
+								<label>Address</label>
+								<textarea value="Smith" rows="3" class="input-xlarge">
+								</textarea>
+			 
+								<div>
+								  <button class="btn btn-primary">Create Account</button>
+								</div>
+							  </form>
+							</div>
+						</div>
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+
     </section>
 			
-			
-<script type="text/JavaScript"language="javascript">
-	//create contact modal
-    // Get the modal
-    var modal = document.getElementById('createGoalModal');
-    // Get the button that opens the modal
-    var btn = document.getElementById("createGoalBtn");
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("closeCreateGoal")[0];
-
-    // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-    
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-	</script>
-    <footer>
+    <!--footer>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
@@ -154,51 +200,8 @@
                 </div>
             </div>
         </div>
-    </footer>
+    </footer-->
        
-<script type="text/JavaScript"language="javascript">
-	$(aclick).click(function() {
-		var $selGoal_id=$(this).siblings().children();		
-		var $correctChild=$selGoal_id[0].value;
-		$(selectedGoal_id).val($correctChild);
-		document.getElementById("hForm").target='_blank';  
-		document.getElementById("hForm").action="././habitual.php";
-		document.getElementById("hForm").submit();
-	});
-	
-	
-	$(quantitativeLabel).click(function(){
-		if (($(gdInputDiv).css('visibility') === 'hidden')&&($(tnInputDiv).css('visibility') === 'hidden')) {
-			$(gdInputDiv).css('visibility', 'visible');
-			$(tnInputDiv).css('visibility', 'visible');
-			$(sdInputDiv).css('visibility', 'visible');
-		}
-	});
-
-
-	$(habitualLable).click(function(){
-		if (($(gdInputDiv).css('visibility') === 'visible')&&($(tnInputDiv).css('visibility') === 'visible')) {
-			$(gdInputDiv).css('visibility', 'hidden');
-			$(tnInputDiv).css('visibility', 'hidden');
-			$(sdInputDiv).css('visibility', 'hidden');
-			$(gdInput).val('');
-			$(tnInput).val('');
-			$(sdInput).val('');
-		}
-	});	
- 
-</script>
-<script type="text/JavaScript"language="javascript">
-	$(bclick).click(function() {
-		var $selGoal_id=$(this).siblings().children();		
-		var $correctChild=$selGoal_id[0].value;
-		$(selectedGoal_id).val($correctChild);
-		document.getElementById("hForm").target='_blank';  
-		document.getElementById("hForm").action="././quantitative.php";
-		document.getElementById("hForm").submit();
-	});
- 
-</script>
     <script src="js/jquery.js"></script>
 
     <script src="js/bootstrap.min.js"></script>
