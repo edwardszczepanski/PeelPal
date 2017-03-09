f125b8f0238f527f6bec60852b4fd8b79c1f73ce
Zhou Li <zhoul@ix-xenial.cs.uoregon.edu>
2017-03-09 15:21:39 -0800

    fix habitual goal's progress bar display error

--- habitual.php
+++ habitual.php
@@ -182,7 +182,7 @@ $selectedGoal_id=$_POST['selectedGoal_id'];
 								    
 			<?php
 			
-			$stmt = $mysqli -> prepare("SELECT g.goal_id, g.g_name, g.goal_type, g.last_act, DAYOFYEAR(g.last_act), DAYOFYEAR(g.startDate), nihao.numProg, TIMESTAMPDIFF (DAY,g.startDate,g.last_act) AS day_diff FROM goal g LEFT JOIN (SELECT goal_id,g_name,goal_type,last_act,DAYOFYEAR(last_act), DAYOFYEAR(startDate), COUNT(*) AS numProg, TIMESTAMPDIFF (DAY,goal.startDate,goal.last_act) AS day_diff FROM goal join contribution c on(goal.goal_id=c.g_id) WHERE c.evaluate='positive' AND g_state=0 AND goal_type=0 GROUP BY goal_id) AS nihao ON g.goal_id = nihao.goal_id WHERE g.goal_id = $selectedGoal_id; ");
+			$stmt = $mysqli -> prepare("SELECT g.goal_id, g.g_name, g.goal_type, g.last_act, DAYOFYEAR(g.last_act), DAYOFYEAR(g.startDate), nihao.numProg, TIMESTAMPDIFF (DAY,g.startDate,CURDATE()) AS day_diff FROM goal g LEFT JOIN (SELECT goal_id,g_name,goal_type,last_act,DAYOFYEAR(last_act), DAYOFYEAR(startDate), COUNT(*) AS numProg, TIMESTAMPDIFF (DAY,goal.startDate,CURDATE()) AS day_diff FROM goal join contribution c on(goal.goal_id=c.g_id) WHERE c.evaluate='positive' AND g_state=0 AND goal_type=0 GROUP BY goal_id) AS nihao ON g.goal_id = nihao.goal_id WHERE g.goal_id = $selectedGoal_id; ");
 			$stmt->execute();
 			$goal_id=null;
 			$goal_name=null;
